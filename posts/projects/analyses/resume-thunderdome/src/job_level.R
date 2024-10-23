95c554b6f0e8533421efa7b6763516f1611cd5c7
hail_hydra <fsu.santiago.rodriguez@gmail.com>
2024-10-22 21:18:44 -0500

    post:projects:analyses:resume thunderdome - new dir src for fucns; imported data, prepped

---
+++ posts/projects/analyses/resume-thunderdome/src/job_level.R
@@ -0,0 +1,34 @@
+#' ...
+#' 
+#' Creates two columns:
+#'   - job family
+#'   - job level
+
+# analyst
+# - vp                5
+# - senior principal  4
+# - principal         3
+# - lead              2
+# - analyst           1
+
+# ds (ic)
+# - senior principal            5
+# - principal, staff            4
+# - lead, midlevel, iii         3
+# - senior data scientist, ii   2
+# - data scientist, i           1
+
+# ml engineer
+# - senior: principal, staff    5
+# - principal, staff            4
+# - lead                        3
+# - senior                      2
+# - ml engineer                 1
+
+# leadership
+# - vp                                              6
+# - senior director, head director, director iii    5
+# - director, director ii                           4
+# - group manager, associate director, director i   3
+# - senior manager                                  2
+# - manager                                         1
